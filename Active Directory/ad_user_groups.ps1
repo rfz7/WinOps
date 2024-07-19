b2ee6f1fdd19fd9784d77b3d7f147d254dec155a
panin.dv <dmitriy.v.panin@sbermarket.ru>
2024-07-19 19:21:33 +0300

    [AD] user groups

---
+++ Active Directory/ad_user_groups.ps1
@@ -0,0 +1,24 @@
+<#
+    User groups
+#>
+
+$Users = Get-ADUser -Filter { Enabled -eq $false -and name -notlike "svc.*" } -Properties *
+
+$Properties = @{"login" = ""; "email" = ""; "display_name" = ""; "disable_date" = ""; "groups" = "" }
+$UserGroups = @()
+
+foreach ($user in $users) {
+    $i = $null
+    $i = Get-ADPrincipalGroupMembership $user | select name
+    $obj = $null
+    $obj = New-Object -TypeName psobject -Property $properties
+    $obj.login = [string] $user.SamAccountName
+    $obj.email = [string] $user.mail
+    $obj.display_name = [string] $user.DisplayName
+    $obj.disable_date = [string] $user.whenChanged
+    $obj.groups = [array] $i.name
+    $UserGroups += $obj
+    Write-Host $user.name -ForegroundColor Yellow
+}
+
+$UserGroups | Export-Csv -Path C:\temp\1.csv -Encoding UTF8
